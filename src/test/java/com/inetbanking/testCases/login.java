7194f663637fccdaf2767d310b71887c85ebe3fe
Kartik01rate <73992510+Kartik01rate@users.noreply.github.com>
2022-11-15 11:43:17 +0530

    1st commit

---
+++ src/test/java/com/inetbanking/testCases/login.java
@@ -0,0 +1,26 @@
+package com.inetbanking.testCases;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+public class login {
+public static void main(String[] args) throws Exception {
+System.setProperty("webdriver.chrome.driver", "C:\\Users\\KARTIK'S\\git\\kartik_cicd_pract\\Drivers\\chromedriver.exe");
+WebDriver driver=new ChromeDriver();
+driver.manage().window().maximize();
+driver.get("https://www.browserstack.com/users/sign_in");
+Thread.sleep(5000);
+WebElement username=driver.findElement(By.xpath("//*[@id='user_email_login']"));
+WebElement password=driver.findElement(By.id("user_password"));
+WebElement login=driver.findElement(By.name("commit"));
+username.sendKeys("abc@gmail.com");
+password.sendKeys("your_password");
+login.click();
+String actualUrl="https://live.browserstack.com/dashboard";
+String expectedUrl= driver.getCurrentUrl();
+Assert.assertEquals(expectedUrl,actualUrl);
+System.out.println("login success");
+}
+}
