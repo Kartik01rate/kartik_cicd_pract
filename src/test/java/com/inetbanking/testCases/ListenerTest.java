c3be665bdc186b79037c980f6d10f5e01570523b
Kartik01rate <73992510+Kartik01rate@users.noreply.github.com>
2022-12-08 12:16:14 +0530

    added sc method

--- src/test/java/com/inetbanking/testCases/ListenerTest.java
+++ src/test/java/com/inetbanking/testCases/ListenerTest.java
@@ -1,9 +1,12 @@
 package com.inetbanking.testCases;
 
+import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+
+import okhttp3.internal.cache.DiskLruCache.Snapshot;
 
 public class ListenerTest implements ITestListener
 {
@@ -29,8 +32,18 @@ public class ListenerTest implements ITestListener
 
 	    @Override		
 	    public void onTestFailure(ITestResult arg0) {					
-	        // TODO Auto-generated method stub				
-	        		
+	        // TODO Auto-generated method stub	
+	    	Screenshot sc=new Screenshot();
+	    	WebDriver webdriver = null;
+			String file = null;
+			
+				try {
+					sc.takeSnapShot(webdriver, file);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			
 	    }		
 
 	    @Override		
